1689557cfbcefe816af62f5f2fccdc10401f2a6f
CeMagnum <90687423+CeMagnum@users.noreply.github.com>
2022-05-11 14:12:02 +0200

    start

---
+++ includes/footer.php
@@ -0,0 +1,2 @@
+<footer>Ik wil ook een footer ik ben ook een klant. Neem contact op <a class="navbar_link" href="../public/contact.php">Contact</a></footer>
+</html>
---
+++ public/journey.php
@@ -0,0 +1,25 @@
+<?php include('../includes/header.php'); ?>
+
+<div class="navbar_main_div">
+    <div class="navbar_middle_div">
+        <a class="navbar_link" href="../index.php"><button class="navbar_butn">Home</button></a>
+        <a class="navbar_link" href=""><button class="navbar_butn">log in</button></a>
+    </div>
+</div>
+<div class="journey_base_div">
+    <div class="journey_div">
+        <div class="journey_img">
+
+        </div>
+        <div class="journey_txt"></div>
+        <div class="journey_buttons"></div>
+    </div>
+</div>
+
+
+
+
+
+
+
+<?php include('../includes/footer.php'); ?>
