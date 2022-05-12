3bfa2c604ea35b086b341a05bce328e7e1f0ece7
CeMagnum <90687423+CeMagnum@users.noreply.github.com>
2022-05-12 14:39:18 +0200

    Read journeys

--- includes/connect.php
+++ includes/connect.php
@@ -2,7 +2,7 @@
     try {
     $conn = new PDO("mysql:host=$servername;dbname=unst", "root", "");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // echo "Connected successfully";
+    echo "Connected successfully";
         } catch(PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
         }
