c17839f7da8780b42c8814bd7d7af8995beebcdf
CeMagnum <90687423+CeMagnum@users.noreply.github.com>
2022-05-12 14:47:10 +0200

    removed a bug

--- includes/connect.php
+++ includes/connect.php
@@ -2,8 +2,8 @@
     try {
     $conn = new PDO("mysql:host=$servername;dbname=unst", "root", "");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
+    // echo "Connected successfully";
         } catch(PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
+    echo "Connection to database failed: " . $e->getMessage();
         }
 ?>
