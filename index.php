1b6ea0a39badf9040966e7f8b1816e93baa2b97a
Draagn282 <daanpx@hotmail.com>
2022-05-13 09:02:27 +0200

    update 13-5-2022

--- index.php
+++ index.php
@@ -3,8 +3,22 @@
 <link rel="icon" type="image/x-icon" href="../assets/img/favicon.ico">
 <body>
 <a class="navbar_link" href="../index.php"><button class="navbar_butn">Home</button></a>
+<div class="navbar_main_div">
+    <div class="navbar_middle_div">
+        <a class="navbar_link" href="../index.php"><button class="navbar_butn">Home</button></a>
         <a class="navbar_link" href=""><button class="navbar_butn">Plan your great journey</button></a>
         <a class="navbar_link" href=""><button class="navbar_butn">log in</button></a>
+    </div>
+</div>
+<div class="navbar_space"></div>
+<div class="home_landing_title">
+   <h1>Welcome to United Nations Travels</h1>
+    <p>
+        We are very pleased for picking our flight industry. We know that we are one of fewe so we try to get you to the best places as save as possible
+        we have verry good transportation.
+    </p> 
+</div>
+
     <h1>Welcome to United Nations Space Travel</h1>
     <h2><a href="public/journey.php">Join the great journey!</a></h2>
 </body>
