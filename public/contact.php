8ab2a0608a540fbf2ce173efce8667a1ba8cb823
Draagn282 <daanpx@hotmail.com>
2022-05-18 13:43:18 +0200

    update 18-5-2022

--- public/contact.php
+++ public/contact.php
@@ -1,91 +1,85 @@
 <?php include('../includes/header.php'); ?>
 <?php include("../includes/navbar.php")?>
 <div class="contact_main_div">
-    <div class="contact_info_div">
-        <div class="contact_info_txt_div">
-            <div class="contact_info_title">
-                <p>
-                    waar wij begonen
-                </P>
-            </div>
-            <div class="contact_info_txt">
-                <p>
-                    aliens came and went on to kill us. they kept screaming about this great journey thing. it kinda annoyed us so we startet kidnapping chilldren and turning them into crybabys zus they dont like thier helmets or something
-                </P>
-            </div>
-        </div>
-        <div class="contact_info_txt_div">
-            <div class="contact_info_title">
-                <p>
-                    waarom wij doorgaan
-                </P>
-            </div>
-            <div class="contact_info_txt">
-                <p>
-                    well the destruction of humanity is not so poggers
-                </P>
-            </div>
-        </div>
-        <div class="contact_info_txt_div">
-            <div class="contact_info_title">
-                <p>
-                    waarom jij met ons mee moet doen
-                </P>
-            </div>
-            <div class="contact_info_txt">
-                <p>
-                    we dying so we need u 
-                </P>
-            </div>
-        </div>
+  <div class="contact_info_div">
+    <div class="contact_info_txt_div">
+      <div class="contact_info_title">
+        <p>waar wij begonen</p>
+      </div>
+      <div class="contact_info_txt">
+        <p>
+          aliens came and went on to kill us. they kept screaming about this
+          great journey thing. it kinda annoyed us so we startet kidnapping
+          chilldren and turning them into crybabys zus they dont like thier
+          helmets or something
+        </p>
+      </div>
     </div>
-    <div class="contact_link_div">
-        <div class="contact_link_section">
-            <a class="contact_link_link"><button class="contact_link_butn">link to email</button></a>
-        </div>
-        <div class="contact_link_section">
-            <a class="contact_link_link"><button class="contact_link_butn">link to idk simething to think about</button></a>
-        </div>
-        <div class="contact_link_section">
-            <a class="contact_link_link"><button class="contact_link_butn">link to external website</button></a>
-        </div>
+    <div class="contact_info_txt_div">
+      <div class="contact_info_title">
+        <p>waarom wij doorgaan</p>
+      </div>
+      <div class="contact_info_txt">
+        <p>well the destruction of humanity is not so poggers</p>
+      </div>
     </div>
-    
+    <div class="contact_info_txt_div">
+      <div class="contact_info_title">
+        <p>waarom jij met ons mee moet doen</p>
+      </div>
+      <div class="contact_info_txt">
+        <p>we dying so we need u</p>
+      </div>
+    </div>
+  </div>
+  <div class="contact_link_div">
+    <div class="contact_link_section">
+      <a class="contact_link_link"
+        ><button class="contact_link_butn">link to email</button></a
+      >
+    </div>
+    <div class="contact_link_section">
+      <a class="contact_link_link"
+        ><button class="contact_link_butn">
+          link to idk simething to think about
+        </button></a
+      >
+    </div>
+    <div class="contact_link_section">
+      <a class="contact_link_link"
+        ><button class="contact_link_butn">link to external website</button></a
+      >
+    </div>
+  </div>
 </div>
 <div class="contact_message_div">
-        <div class="contact_message_mail">
-            <div class="contact_mail_name_div">
-                <div class="contact_mail_name">
-                    <p>
-                        tell us your name
-                    </p>
-                </div>
-                <div class="contact_mail_name_input">
-                    <input type="text">
-                </div>
-            </div>
-            <div class="contact_mail_message_div">
-                <div class="contact_mail_message_txt">
-                    <p>
-                        give me ur souls
-                    </p>
-                </div>
-                <div class="contact_mail_message_txt_input">
-                    <textarea></textarea>
-                </div>           
-            </div>
-            <div class="contact_mail_mail_div">
-                <div class="contact_mail_mail_txt">
-                    <p>
-                        giv us ur mail to giv u back ur children
-                    </p>
-                </div>
-                <div class="contact_mail_mail_txt_input">
-                    <input type="text">
-                </div>              
-            </div>
-        </div>
+  <div class="contact_message_mail">
+    <div class="contact_mail_name_div">
+      <div class="contact_mail_name">
+        <p>tell us your name</p>
+      </div>
+      <div class="contact_mail_name_input">
+        <input type="text" placeholder="type your name here" />
+      </div>
+    </div>
+    <div class="contact_mail_message_div">
+      <div class="contact_mail_message_txt">
+        <p>give me ur souls</p>
+      </div>
+      <div class="contact_mail_message_txt_input">
+        <textarea placeholder="type somehitng here"></textarea>
+      </div>
+    </div>
+    <div class="contact_mail_mail_div">
+      <div class="contact_mail_mail_txt">
+        <p>enter your mail</p>
+      </div>
+      <div class="contact_mail_mail_txt_input">
+        <input type="text" placeholder="type your email here" />
+      </div>
+    </div>
+  </div>
 </div>
-    <div class="contact_buttonto_div"></div>
+<div class="contact_buttonto_div"></div>
 
 <?php include('../includes/footer.php'); ?>
