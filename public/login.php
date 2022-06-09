d73e31323a2ca6faf9a73515580e23553f872599
Draagn282 <daanpx@hotmail.com>
2022-06-09 16:18:13 +0200

    update 9-6-2022

--- public/login.php
+++ public/login.php
@@ -1,6 +1,7 @@
 <?php include("../includes/header.php"); include("../includes/navbar.php") ?>
 <body>
     <div class="login_div">
+      
       <div class="loggin_main">
         <div class="loggin_txt">
           <p>Log in</p>
@@ -8,23 +9,28 @@
         <div class="loggin_form">
           <form class="loggin_log" action="../functions/login.php" method="post"> 
             <label for="name">Username:</label><br>
-            <input type="text" id="name" name="username" autocomplete="off"><br>
+            <input type="text" id="name" name="username" autocomplete="off" placeholder="username here" ><br>
             <label for="pass">Password:</label><br>
             <input type="password" id="pass" name="password" autocomplete="off"><br>
             <input type="submit" id="button" name="login" value="Login">
           </form>  
       </div>
     </div>
-    <div></div>
-    <form class="loggin_log" action="../functions/register.php" method="post"> 
-            <label for="name">Register<br>Username:</label><br>
+    <div class="loggin_main">
+      <div class="loggin_txt">
+        <p>Register</p>
+      </div>
+      <form class="loggin_log" action="../functions/register.php" method="post"> 
+            <label>Username:</label><br>
             <input type="text" id="name" name="username" autocomplete="off"><br>
             <label for="name">Email:</label><br>
             <input type="text" id="email" name="email" autocomplete="off"><br>
             <label for="pass">Password:</label><br>
             <input type="password" id="pass" name="password" autocomplete="off"><br>
             <input type="submit" id="button" name="Register" value="Register">
-    </form>  
+      </form> 
+    </div>
+ 
 </div>
 <!-- <script src="../assets/js/fieldvalidation.js"></script> -->
 </body>
