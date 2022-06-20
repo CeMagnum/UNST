3863fe62a2adf681bfde6c261f8d7b41f23309c0
CeMagnum <90687423+CeMagnum@users.noreply.github.com>
2022-06-20 14:21:01 +0200

    pointer fix

--- admin/includes/admin_session.php
+++ admin/includes/admin_session.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
     if(!isset($_SESSION['admin'])){
-        header('location: ../public/login.php');
+        header('location: ../../public/login.php');
         exit();
     }
 ?>
