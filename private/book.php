ba94d03f2451f7848aad00d45c8b198991032415
CeMagnum <90687423+CeMagnum@users.noreply.github.com>
2022-06-20 14:53:35 +0200

    minor changes

--- private/book.php
+++ private/book.php
@@ -2,6 +2,7 @@
 include("../includes/connect.php");
 include("../includes/header.php"); 
 include("../includes/navbar.php");
+include('./user_session.php');
 ?>
 <title>UNST Book</title>
 <link rel="icon" type="image/x-icon" href="../assets/img/logo2_small_WIP.png">
