43ed587c325a331327ef0150615ad99305cc9582
CeMagnum <90687423+CeMagnum@users.noreply.github.com>
2022-06-28 10:55:01 +0200

    minor changes

--- assets/js/scrollfunciton.js
+++ assets/js/scrollfunciton.js
@@ -1,24 +1,3 @@
-// var mybutton = document.getElementById("myBtn");
-
-// window.onscroll = function () {
-//   scrollFunction();
-// };
-
-// function scrollFunction() {
-//   if (document.body.scrollTop > 50 || document.body.scrollTop < 50) {
-//     mybutton.style.display = "block";
-//     $(".navbar_main_div").addClass(".sticky_nav");
-//     $(".navbar_butn").addClass(".ticky_butn");
-//   } else {
-//     // mybutton.style.display = "none";
-//     $(".navbar_main_div").removeClass(".sticky_nav");
-//     $(".navbar_butn").removeClass(".sticky_butn");
-//   }
-// }
-// window.onscroll = function () {
-//   scrollFunction();
-// };
-
 $(document).ready(function () {
   $(window).scroll(function () {
     // sticky navbar on scroll script
